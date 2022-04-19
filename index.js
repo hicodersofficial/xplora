759e299e0434e47e01ac5964581913d608aac9cf
HiCoders <hicodersofficial@gmail.com>
2022-04-19 23:50:30 +0530

    fixed replaceAll method

--- index.js
+++ index.js
@@ -7,4 +7,11 @@
 
 "use strict";
 
+// Replaces all match from string.
+// NOTE: this is the fixed for linux.
+String.prototype.replaceAll = function (match, val) {
+  const regex = new RegExp(match, "g");
+  return this.replace(regex, val);
+};
+
 module.exports = require("./lib/xplora");
